09f63d018a14e1f9b83362bdd4838fa75a7c47a2
Antonio González <antgon@cantab.net>
2021-07-13 13:41:55 +0100

    Renamed set_autoconfig() to autoconfig()

--- lib/mpr121.c
+++ lib/mpr121.c
@@ -124,7 +124,7 @@ void mpr121_init(void) {
     mpr121_write(MPR121_ELECTRODE_CONFIG, 0xCF);
 }
 
-bool mpr121_set_autoconfig(void){
+bool mpr121_autoconfig(void){
     // Read current configuration and then enter stop mode. Stop mode
     // is needed because register write operations can only be done in
     // this mode.
@@ -172,13 +172,11 @@ bool mpr121_set_autoconfig(void){
     // Check to see if autoconfiguration worked. Bits 7-6 in ELE-8
     // out-of-range register are flags that are set when
     // auto-(re)configuration fails.
-    bool autoconf_failed = false;
+    bool success = false;
     uint8_t out_of_range_status;
     mpr121_read(MPR121_OUT_OF_RANGE_STATUS_1, &out_of_range_status);
     if ((out_of_range_status & 0b11000000) == 0){
-        autoconf_failed = false;
-    } else {
-        autoconf_failed = true;
+        success = true;
     }
-    return autoconf_failed;
+    return success;
 }
