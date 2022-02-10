8c958cab328948bfbf3276066e05f58d92e110f9
Antonio González <antgon@cantab.net>
2022-02-10 10:04:23 +0000

    Default values match those on datasheet
    Default values used to initialise the MPR121 sensor (function
    `mpr1221_init`) were set to match those listed in the application note
    MPR121 Quick Start Guide (AN3944 Rev 1). I also updated the code
    comments to have more information about all the differnt options.

--- lib/mpr121.c
+++ lib/mpr121.c
@@ -31,22 +31,22 @@ void mpr121_init(i2c_inst_t *i2c_port, uint8_t i2c_addr,
     // non-noise drift is detected. The range of the effective value is
     // 1~63.
     mpr121_write(MPR121_NOISE_HALF_DELTA_RISING_REG, 0x01, sensor);
-    mpr121_write(MPR121_NOISE_HALF_DELTA_FALLING_REG, 0x05, sensor);
+    mpr121_write(MPR121_NOISE_HALF_DELTA_FALLING_REG, 0x01, sensor);
     mpr121_write(MPR121_NOISE_HALF_DELTA_TOUCHED_REG, 0x00, sensor);  
     
     // Noise Count Limit (NCL): Determines the number of samples
     // consecutively greater than the Max Half Delta value. This is
     // necessary to determine that it is not noise. The range of the
     // effective value is 0~255.
-    mpr121_write(MPR121_NOISE_COUNT_LIMIT_RISING_REG, 0x0E, sensor);
-    mpr121_write(MPR121_NOISE_COUNT_LIMIT_FALLING_REG, 0x01, sensor);
+    mpr121_write(MPR121_NOISE_COUNT_LIMIT_RISING_REG, 0x00, sensor);
+    mpr121_write(MPR121_NOISE_COUNT_LIMIT_FALLING_REG, 0xff, sensor);
     mpr121_write(MPR121_NOISE_COUNT_LIMIT_TOUCHED_REG, 0x00, sensor);
     
     // Filter Delay Count Limit (FDL): Determines the operation rate of
     // the filter. A larger count limit means the filter delay is
     // operating more slowly. The range of the effective value is 0~255.
     mpr121_write(MPR121_FILTER_DELAY_COUNT_RISING_REG, 0x00, sensor);
-    mpr121_write(MPR121_FILTER_DELAY_COUNT_FALLING_REG, 0x00, sensor);
+    mpr121_write(MPR121_FILTER_DELAY_COUNT_FALLING_REG, 0x02, sensor);
     mpr121_write(MPR121_FILTER_DELAY_COUNT_TOUCHED_REG, 0x00, sensor);
 
     // Debounce. Value range for each is 0~7.
@@ -60,42 +60,65 @@ void mpr121_init(i2c_inst_t *i2c_port, uint8_t i2c_addr,
 
     // Global filter and charge/discharge settings
 
-    // Register 0x5C (AFE configuration register)
+    // Register 0x5C: Filter/global CDC configuration register
     //
     // First filter iterations (FFI), bits 7-6. Number of samples taken
-    // as input to the first level of filtering. Default is 0b00 = sets
-    // samples taken to 6.
+    // as input to the first level of filtering.
+    //     00 - Sets samples taken to 6 (default)
+    //     01 - Sets samples taken to 10
+    //     10 - Sets samples taken to 18
+    //     11 - Sets samples taken to 34
     //
     // Charge-discharge current (CDC), bits 5-0. Sets the value of
-    // charge-discharge current. Max is 63 µA in 1 µA steps. Default is
-    // 16 (0b010000) = 16 µA.
+    // charge-discharge current applied to the electrode. Max is 63 µA
+    // in 1 µA steps. Default is
+    //     000000 - Disable electrode charging
+    //     000001 - Sets the current to 1 µA
+    //     ~ 
+    //     010000 - Sets the current to 16 µA (default)
+    //     ~
+    //     111111 - Sets the current to 63 µA
     //
-    // Defaults = 0b00010000 = 0x10
+    // AFE configuration register default = 0b00010000 = 0x10
     mpr121_write(MPR121_AFE_CONFIG_REG , 0x10, sensor);
 
-    // Register 0x5D (Filter configuration register)
+    // Register 0x5D: Filter/global CDC configuration register
     //
     // Charge discharge time (CDT), bits 7-5. Selects the global value
     // of charge time applied to electrode. The maximum is 32 μs,
-    // programmable as 2 ^(n-2) μs. Default is 0b001 = time set to 0.5
-    // µs.
+    // programmable as 2^(n-2) μs.
+    //     000 - Disables electrode charging
+    //     001 - Time is set to 0.5 µs (default)
+    //     010 - Time is set to 1 µs
+    //     ~
+    //     111 - Time is set to 32 µs
     //
     // Second filter iterations (SFI), bits 4-3. Selects the number of
-    // samples taken for the second level filter. Default is 0b00 =
-    // number of samples is set to 4.
+    // samples taken for the second level filter.
+    //     00 - Number of samples is set to 4 (default)
+    //     01 - Number of samples is set to 6
+    //     10 - Number of samples is set to 10
+    //     11 - Number of samples is set to 18
     //
-    // Electrode sample interval (ESI), bits 2-0. Selects the period
-    // between samples used for the second level of filtering. The
-    // maximum is 128 ms, programmable to 2^n ms. Default is 0b100
-    // (encoding 4) = 16 ms.
+    // Electrode sample interval (ESI), bits 2-0. Controls the sampling
+    // rate of the device. The maximum is 128 ms, programmable to 2^n
+    // ms.  Decrease this value for better response time, increase to
+    // save power.
+    //     000 - Period set to 1 ms
+    //     001 - Period set to 2 ms
+    //     ~
+    //     100 - Period set to 16 ms (default)
+    //     ~
+    //     111 - Period set to 128 ms
     // 
-    // Defaults = 0b00100100 = 0x24
-    mpr121_write(MPR121_FILTER_CONFIG_REG, 0x24, sensor);
+    // Filter configuration register default = 0b00100100 = 0x24
+    // Quick Start Guide (AN3944) suggests 0x04.
+    mpr121_write(MPR121_FILTER_CONFIG_REG, 0x04, sensor);
 
     // Electrode configuration (register 0x5E).
     //
-    // Determines if the MPR121 is in Run Mode or Stop Mode, controls the
-    // baseline tracking operation and specifies the input
+    // Determines if the MPR121 is in Run Mode or Stop Mode, controls
+    // the baseline tracking operation and specifies the input
     // configurations of the 13 channels.
 
     // Enter run mode: enable all electrodes. The electrode
@@ -106,16 +129,38 @@ void mpr121_init(i2c_inst_t *i2c_port, uint8_t i2c_addr,
     // capacitance change to the input sensing channels. Enabled by
     // default (value 0b00). Value 0b11 enables tracking and also loads
     // initial value from electrode data.
+    //     00 - Baseline tracking enabled, initial baseline value is
+    //          current value in baseline value register (default)
+    //     01 - Baseline tracking disabled
+    //     10 - Baseline tracking enabled, initial baseline value is
+    //          loaded with the 5 high bits of the first 10-bit
+    //          electrode data value
+    //     11 - Baseline tracking enabled, initial baseline value is
+    //          loaded with all 10 bits of the first electrode data
+    //          value
     //
     // Proximity enable (ELEPROX_EN), bits 5-4. Default = 0b00
     // (disabled).
+    //     00 - Proximity detection is disabled (default)
+    //     01 - Run mMode with ELE0~ELE1 combined for proximity
+    //          detection enabled
+    //     10 - Run Mode with ELE0~ELE3 combined for proximity
+    //          detection enabled
+    //     11 - Run Mode with ELE0~ELE11 combined for proximity
+    //          detection enabled
     //
     // Electrode enable (ELE_EN), bits 3-0. Sets Run Mode and controls
-    // the operation of the 12 electrodes. Default = 0b1111 = Run Mode
-    // for all 12 electrodes.
+    // the operation of the 12 electrodes detection.
+    //    0000 - Electrode detection is disabled (default)
+    //    0001 - Run Mode with ELE0 for electrode detection enabled
+    //    ~
+    //    1011 - Run Mode with ELE0~ELE10 for electrode detection
+    //           enabled
+    //    11xx - Run Mode with ELE0~ELE11 for electrode detection
+    //           enabled
     //
-    // Value 0b00001111 = 0x0F Value 0b11001111 = 0xCF
-    mpr121_write(MPR121_ELECTRODE_CONFIG_REG, 0xCF, sensor);
+    // ELE_EN default value, 0x0C, which enables all 12 electrodes
+    mpr121_write(MPR121_ELECTRODE_CONFIG_REG, 0x0C, sensor);
 }
 
 bool mpr121_autoconfig(mpr121_sensor_t *sensor){
